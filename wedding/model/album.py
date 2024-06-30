aab9f8fb4f0b7300a861b06a42be59054a7f2ee6
David Manolitsas <davidmanolitsas@icloud.com>
2024-06-30 13:26:38 +1000

    add pages navigation bar

---
+++ wedding/model/album.py
@@ -0,0 +1,7 @@
+
+class Album:
+
+    def __init__(self, template: str, target: str, source: str):
+        self.template = template
+        self.target = target
+        self.source = source
--- wedding/util/photos_util.py
+++ wedding/util/photos_util.py
@@ -3,4 +3,5 @@ import os
 
 def all_photos(path: str):
     for photo in os.scandir(path):
-        yield photo
+        if photo.path.endswith(".jpg") or photo.path.endswith(".JPG"):
+            yield photo
